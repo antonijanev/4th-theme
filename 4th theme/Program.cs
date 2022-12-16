363106928d4403328fee0daefdc82e740bd17ee5
Lenovo <Lenovo@LENOVO-PC>
2022-12-16 09:23:56 +0200

    Add project files.

---
+++ 4th theme/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Paprašyti vartotojo įvesti bet kokį skaičių. Išvesti skaičių sumą nuo 1 iki įvesto skaičiaus.
+
+int nmbr = 0;
+
+for (int i = 0; ; i + int nmbr)
+{
+    Console.WriteLine("Enter a number: ");
+    int nmbr = Convert.ToInt32(Console.ReadLine());
+
+}
